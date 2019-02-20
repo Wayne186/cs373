9951dd1cdce6f5d1715f70ccb12a7372de901030
Wayne186 <wayne186645@gmail.com>
2019-02-20 23:02:08 +0800

    ...

--- hw3/ratingpred.py
+++ hw3/ratingpred.py
@@ -1,17 +1,16 @@
 import numpy as np
 
 # Input: number of labels k
-#        vector theta of d rows, 1 column
-#        vector b of k-1 rows, 1 column
-#        vector x of d rows, 1 column
+# 		vector theta of d rows, 1 column
+# 		vector b of k-1 rows, 1 column
+# 		vector x of d rows, 1 column
 # Output: label (1 or 2 ... or k)
 def run(k,theta,b,x):
-    # Your code goes here
-    label = 0;
-    for i in range(len(b)):
-        if ( np.dot(theta, x) < b[i]):
-            label = i + 1
-            break
-        else:
-            label = k
-    return label
+	compare = (np.dot(x, theta))
+#	print(compare)
+
+	for index in range(len(b)):
+		# print(b[index])
+		if (compare <= b[index]):
+			return index + 1
+	return k
